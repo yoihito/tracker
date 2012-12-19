1e3f7ffba02e5ef129b67a198e36d66d4022e189
yoihito <v1m@tut.by>
2012-12-19 15:23:52 +0300

    save.php

---
+++ save.php
@@ -0,0 +1,10 @@
+<?php
+//error_reporting(E_ALL);
+//ini_set('display_errors','1');
+$x = $_GET['x'];
+$y = $_GET['y'];
+$file = fopen("coords","w");
+if (!$file) echo "unsuccess";
+fprintf($file,"%s %s",$x,$y);
+fclose($file);
+?>
