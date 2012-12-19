f47d165157db506211a76cd1f7e74f79ff10f489
yoihito <v1m@tut.by>
2012-12-19 17:57:37 +0400

    change save.php

--- save.php
+++ save.php
@@ -1,7 +1,12 @@
 <?php
+//error_reporting(E_ALL);
+//ini_set('display_errors','1');
 $x = $_GET['x'];
 $y = $_GET['y'];
-$file = fopen("coord.txt","w");
-fprintf($file,"%s\n%s",$x,$y);
+$file = fopen("coords","w");
+if (!$file) echo "unsuccess";
+fprintf($file,"%s %s",$x,$y);
 fclose($file);
+echo "success ${x} ${y}";
+
 ?>
