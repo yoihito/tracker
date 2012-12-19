2b2488aafe1193fb12fc864b58d79e9db1966ef1
yoihito <v1m@tut.by>
2012-12-19 16:16:09 +0300

    commit

--- load.php
+++ load.php
@@ -3,6 +3,6 @@ error_reporting(E_ALL);
 ini_set('display_errors','1');
 $file = fopen("coords","r");
 fscanf($file,"%s %s",$x,$y);
-echo $x.' '.$y;
+echo json_encode(array("lat"=>$x,"lng"=>$y));
 fclose($file);
 ?>
